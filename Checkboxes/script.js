ed565a0e136617b323998e52f1560b81df6d6d6b
duman <duman200354@mail.ru>
2022-07-01 23:10:59 +0600

    checkboxes done

---
+++ Checkboxes/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const checkboxes = document.querySelectorAll('.wrapper input[type="checkbox"');
+const labels = document.querySelector('.item p');
+
+let lastChecked;
+
+function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if ( checkbox === this || checkbox === lastChecked ) {
+                inBetween = !inBetween;
+            }
+            if ( inBetween ) {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
